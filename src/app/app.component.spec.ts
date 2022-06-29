751358a680681b796007aa05c4a5dbebd99cc28d
Juan Pablo Muñoz <munozsjp@gmail.com>
2022-06-29 21:34:33 +0200

    modify app.component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,7 +19,7 @@ describe('AppComponent', () => {
   it(`should have as title 'frontend'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('frontend');
+    expect(app.title).toEqual('DevOps');
   });
 
   it('suma 2+2 = 4', () => {
