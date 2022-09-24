aa06818eed538a32b991033b9bc47d58671b542a
Juan Pablo Muñoz <munozsjp@gmail.com>
2022-09-24 23:48:00 +0200

    new test(success)

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,7 +19,7 @@ describe('AppComponent', () => {
   it(`should have as title 'frontend'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('DevOps');
+    expect(app.title).toEqual('DevSecOps');
   });
 
   it(`should be true`, () => {
