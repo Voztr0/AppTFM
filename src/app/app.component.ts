fd9062a3960b4d0a784f3a39bd8a833a26f800c6
Juan Pablo Muñoz <munozsjp@gmail.com>
2022-09-24 23:45:36 +0200

    new test(failure)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'DevOps';
+  title = 'DevSecOps';
 
   public newFunction(value: Number) {
     if (value > 1) {
